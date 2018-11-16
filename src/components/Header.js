681621e7632aa1adfc5deef5e102061487d4d4fe
Jane Costa <yberdysh@gmail.com>
2018-11-16 00:20:23 -0500

    fixed header error

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ import { Header } from 'semantic-ui-react'
 const NavHeader = () => {
   return (
     <Header className='header' as='h5' block>
+    <div>
       <h3> Hashketball: The Dribbling </h3>
       <img className='App-logo' atl='basketball' src='https://images.vexels.com/media/users/3/135385/isolated/lists/212a5985af3fc8329ada7bc2a45bad82-basketball-circle-icon.png'></img>
+      </div>
     </Header>
   )
 }
